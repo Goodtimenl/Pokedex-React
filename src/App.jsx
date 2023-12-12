bbfaf28ac449cdf8560601fb47811e7200a6ef56
Goodtimenl <gregory.delaloeuf@gmail.com>
2023-12-12 10:46:18 +0100

    React Basics 04

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import PokemonCard from './components/PokemonCard';
 import "../src/App.css"
 
-const App = () => {
+
+function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
-    <div>
-      <h1>Mon Application Pokémon</h1>
-      <PokemonCard />
+    <div className="app">
+      {/* ... */}
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
-};
+}
+
+
+
 
 export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
 
-function PokemonCard() {
-	const pokemon = pokemonList[1];
+function PokemonCard(props) {
+	const { pokemon } = props;
 
 	return (
 		<div className="pokemon-card">
 			<h2>{pokemon.name}</h2>
-			{pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) :
-				(<p>???</p>)}
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
 		</div>
 	);
 }
 
 
 
-const pokemonList = [
-	{
-		name: "bulbasaur",
-		imgSrc:
-			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-	},
-	{
-		name: "Mew",
-	},
-];
+
 
 
 export default PokemonCard;
